9c315e071fb0220dc49d5956c27062a533d6194c
khaivuong1998 <khaivuong311098@gmail.com>
2023-07-18 19:16:25 +0700

    first commit

--- bin/exercise2.dart
+++ bin/exercise2.dart
@@ -3,12 +3,12 @@ import 'dart:ffi';
 void main() {
   var colors = ["yellow", "red", "black", "white", "blue"];
   var colors2 = ["green", "pink", "purple", "brown"];
-  List list = List.filled(colors.length, "", growable: true);
+  List<String> list = List.filled(colors.length, "", growable: true);
   for (int i = 0; i < colors.length; i++) {
     list[i] = colors[i];
   }
   print(list);
-  List list2 = List.filled(colors2.length, "", growable: true);
+  List<String> list2 = List.filled(colors2.length, "", growable: true);
   for (int i = 0; i < colors2.length; i++) {
     list2[i] = colors2[i];
   }
@@ -18,9 +18,11 @@ void main() {
   // }
   list.addAll(list2);
   list2.clear();
-
   print(list2);
   print(list);
+  list.forEach((element) {
+    print(element.toUpperCase());
+  });
   list.removeRange(4, 7);
   print(list);
   print(list.reversed);
